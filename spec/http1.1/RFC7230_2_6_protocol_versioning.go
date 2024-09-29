640331c60b9cee9991c87500818f2ed30dd27637
LeaYeh <smile2140@gmail.com>
2024-09-29 19:15:22 +0000

    fix: Update the requirement description

--- spec/http1.1/RFC7230_2_6_protocol_versioning.go
+++ spec/http1.1/RFC7230_2_6_protocol_versioning.go
@@ -11,7 +11,7 @@ func Http11ProtocolVersioning() *spec.TestGroup {
 	tg.AddTestCase(&spec.TestCase{
 		Strict:      true,
 		Desc:        "Assuming the server supports HTTP/1.1 only, request for HTTP/1.1 should work normally",
-		Requirement: "A recipient can assume that a message with a higher minor version, when sent to a recipient that has not yet indicated support for that higher version, is sufficiently backwards-compatible to be safely processed by any implementation of the same major version.",
+		Requirement: "A server MUST NOT send a version to which it is not conformant. A server can send a 505.",
 		Run: func(c *config.Config, conn *spec.Conn) error {
 			passed := true
 			expected := "HTTP/1.1 200 OK\r"
@@ -80,7 +80,7 @@ func Http11ProtocolVersioning() *spec.TestGroup {
 	tg.AddTestCase(&spec.TestCase{
 		Strict:      true,
 		Desc:        "Assuming the server supports HTTP/1.1 only, request for HTTP/1.2 should behave as if it was HTTP/1.1",
-		Requirement: "A recipient can assume that a message with a higher minor version, when sent to a recipient that has not yet indicated support for that higher version, is sufficiently backwards-compatible to be safely processed by any implementation of the same major version.",
+		Requirement: "A server MUST NOT send a version to which it is not conformant. A server can send a 505.",
 		Run: func(c *config.Config, conn *spec.Conn) error {
 			passed := true
 			expected := "HTTP/1.1 200 OK\r"
@@ -116,7 +116,7 @@ func Http11ProtocolVersioning() *spec.TestGroup {
 	tg.AddTestCase(&spec.TestCase{
 		Strict:      true,
 		Desc:        "Assuming the server supports HTTP/1.1 only, request for HTTP/2.0 should behave as if it was HTTP/1.1",
-		Requirement: "A recipient can assume that a message with a higher minor version, when sent to a recipient that has not yet indicated support for that higher version, is sufficiently backwards-compatible to be safely processed by any implementation of the same major version.",
+		Requirement: "A server MUST NOT send a version to which it is not conformant. A server can send a 505.",
 		Run: func(c *config.Config, conn *spec.Conn) error {
 			passed := true
 			expected := "HTTP/1.1 505 HTTP Version Not Supported\r"
@@ -152,7 +152,7 @@ func Http11ProtocolVersioning() *spec.TestGroup {
 	tg.AddTestCase(&spec.TestCase{
 		Strict:      true,
 		Desc:        "Assuming the server supports HTTP/1.1 only, request for HTTP/3.0 should behave as if it was HTTP/1.1",
-		Requirement: "A recipient can assume that a message with a higher minor version, when sent to a recipient that has not yet indicated support for that higher version, is sufficiently backwards-compatible to be safely processed by any implementation of the same major version.",
+		Requirement: "A server MUST NOT send a version to which it is not conformant. A server can send a 505.",
 		Run: func(c *config.Config, conn *spec.Conn) error {
 			passed := true
 			expected := "HTTP/1.1 505 HTTP Version Not Supported\r"
@@ -187,8 +187,73 @@ func Http11ProtocolVersioning() *spec.TestGroup {
 
 	tg.AddTestCase(&spec.TestCase{
 		Strict:      true,
+		Desc:        "The request for HTTP/-1.1 is invalid",
+		Requirement: "A server MUST NOT send a version to which it is not conformant. A server can send a 505.",
+		Run: func(c *config.Config, conn *spec.Conn) error {
+			passed := true
+			expected := "HTTP/1.1 400 Bad Request\r"
+
+			request := "GET / HTTP/-1.1\r\n"
+			err := conn.Send([]byte(request))
+			if err != nil {
+				return err
+			}
+			acturl, err := conn.ReadLine()
+			if err != nil {
+				return err
+			}
+
+			if acturl != expected {
+				passed = false
+			}
+
+			if !passed {
+				return &spec.TestError{
+					Expected: []string{expected},
+					Actual:   acturl,
+				}
+			}
+			return nil
+		},
+	})
+
+
+	tg.AddTestCase(&spec.TestCase{
+		Strict:      true,
+		Desc:        "The request for HTTP/0.0 is invalid",
+		Requirement: "A server MUST NOT send a version to which it is not conformant. A server can send a 505.",
+		Run: func(c *config.Config, conn *spec.Conn) error {
+			passed := true
+			expected := "HTTP/1.1 400 Bad Request\r"
+
+			request := "GET / HTTP/0.0\r\n"
+			err := conn.Send([]byte(request))
+			if err != nil {
+				return err
+			}
+			acturl, err := conn.ReadLine()
+			if err != nil {
+				return err
+			}
+
+			if acturl != expected {
+				passed = false
+			}
+
+			if !passed {
+				return &spec.TestError{
+					Expected: []string{expected},
+					Actual:   acturl,
+				}
+			}
+			return nil
+		},
+	})
+
+	tg.AddTestCase(&spec.TestCase{
+		Strict:      true,
 		Desc:        "The request for HTTP/0.2 is invalid",
-		Requirement: "A recipient can assume that a message with a higher minor version, when sent to a recipient that has not yet indicated support for that higher version, is sufficiently backwards-compatible to be safely processed by any implementation of the same major version.",
+		Requirement: "A server MUST NOT send a version to which it is not conformant. A server can send a 505.",
 		Run: func(c *config.Config, conn *spec.Conn) error {
 			passed := true
 			expected := "HTTP/1.1 400 Bad Request\r"
@@ -220,7 +285,7 @@ func Http11ProtocolVersioning() *spec.TestGroup {
 	tg.AddTestCase(&spec.TestCase{
 		Strict:      true,
 		Desc:        "The request for HTTP/4.2 is invalid",
-		Requirement: "A recipient can assume that a message with a higher minor version, when sent to a recipient that has not yet indicated support for that higher version, is sufficiently backwards-compatible to be safely processed by any implementation of the same major version.",
+		Requirement: "A server MUST NOT send a version to which it is not conformant. A server can send a 505.",
 		Run: func(c *config.Config, conn *spec.Conn) error {
 			passed := true
 			expected := "HTTP/1.1 505 HTTP Version Not Supported\r"
