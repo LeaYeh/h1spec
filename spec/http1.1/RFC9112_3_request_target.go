e7c6f79b235216b12f3d61ca9d4f165b080c85eb
LeaYeh <LeaYeh@users.noreply.github.com>
2024-12-29 16:21:41 +0000

    feat: feat: Add new test case  Missing Host header

---
+++ spec/http1.1/RFC9112_3_request_target.go
@@ -0,0 +1,11 @@
+package http11
+
+import "github.com/LeaYeh/h1spec/spec"
+
+// HTTP11RequestTarget implements tests for RFC9112 Section 3: "Request Target".
+func HTTP11RequestTarget() *spec.TestGroup {
+    tg := NewTestGroup("RFC9112.3", "Request Target")
+    // Add subchapter-level test groups
+    tg.AddTestGroup(HTTP11MissingHostHeader())
+    return tg
+}
