568645fc662c373816eb291e23ebf884ad3fc43a
LeaYeh <LeaYeh@users.noreply.github.com>
2024-12-30 16:01:18 +0000

    feat: feat: Add new test case  Multiple Content-Length values in a request

--- spec/http1.1/RFC7230.go
+++ spec/http1.1/RFC7230.go
@@ -2,10 +2,16 @@ package http11
 
 import "github.com/LeaYeh/h1spec/spec"
 
+// RFC7230 is the main function for the RFC7230 protocol.
+// It creates a new test group for the protocol and adds chapter-level test groups to it.
+// The purpose of this RFC is to define the syntax and semantics of HTTP/1.1 messages, both as they are transmitted on the wire and as they should be in the internal data structures of an HTTP/1.1 implementation.
 func RFC7230() *spec.TestGroup {
 	tg := NewTestGroup("RFC7230", "Protocol RFC7230")
 
-	tg.AddTestGroup(HTTP11Architecture())
+	// DO NOT remove existing test groups, extend it only
+	// Add chapter-level test group for Message Format
+	// Reference Implementation will be in the future files
+	tg.AddTestGroup(HTTP11MessageFormat())
 
 	return tg
 }
