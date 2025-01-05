c703c2861a0b56708d8d0e26cfa0c42afe134839
LeaYeh <LeaYeh@users.noreply.github.com>
2025-01-05 17:03:46 +0000

    feat: feat: Add new test case  Chunk Size can only contain hexadecimal characters (0123456789ABCDEFabcdef)

--- spec/http1.1/RFC9112.go
+++ spec/http1.1/RFC9112.go
@@ -2,15 +2,17 @@ package http11
 
 import "github.com/LeaYeh/h1spec/spec"
 
-// RFC9112 is the main function for the RFC9112 protocol.
-// It creates a new test group for the protocol and adds chapter-level test groups to it.
-// The purpose of RFC9112 is to define the syntax and semantics of HTTP/1.1 request-target.
 func RFC9112() *spec.TestGroup {
 	tg := NewTestGroup("RFC9112", "Protocol RFC9112")
 
-	// Add chapter-level test groups
-	// The implementation of these test groups will be provided in future files
-	tg.AddTestGroup(HTTP11RequestTarget())
+	// IMPORTANT: Preserve existing test groups
+    tg.AddTestGroup(HTTP11RequestTarget())
+
+	// Add chapter-level test group
+	// RFC9112.7: Chunked Transfer Coding
+	// This test group is to verify the implementation of chunked transfer coding in HTTP/1.1
+	// Reference Implementation will be in the future files
+	tg.AddTestGroup(HTTP11ChunkedTransferCoding())
 
 	return tg
 }
